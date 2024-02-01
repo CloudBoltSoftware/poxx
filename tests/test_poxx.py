9a0744f765cd9798e3d08b37f702eb367c217554
E Einowski <eeinowski@cloudbolt.io>
2024-01-31 20:17:38 -0800

    🔧 refactor(test_poxx.py): replace single quotes with double quotes for string literals The change was made to improve code readability and consistency. The Python style guide PEP 8 recommends using double quotes for string literals that contain single quotes to avoid backslashes in the string, making the code cleaner and easier to read.

--- tests/test_poxx.py
+++ tests/test_poxx.py
@@ -16,46 +16,44 @@ from poxx import munge_one_file, diff_one_file
 
 
 class PoxxTestCase(unittest.TestCase):
-
     def setUp(self):
-        self.data_path = os.path.join(TEST_PATH, 'data')
+        self.data_path = os.path.join(TEST_PATH, "data")
 
-        self.sample_po_path = os.path.join(self.data_path, 'sample.po')
-        self.canon_po_path = os.path.join(self.data_path, 'canon.po')
+        self.sample_po_path = os.path.join(self.data_path, "sample.po")
+        self.canon_po_path = os.path.join(self.data_path, "canon.po")
         self.sample_po = polib.pofile(self.sample_po_path)
 
     def tearDown(self):
-
         # go back to where we were
         self.sample_po.save(self.sample_po_path)
 
 
 class TestMungePoFile(PoxxTestCase):
-
     def test_munge(self):
         munge_one_file(self.sample_po_path, blank=False)
         munged_pofile = polib.pofile(self.sample_po_path)
 
-        self.assertEqual(munged_pofile[0].msgstr, u'Ẑḗřǿ')
-        self.assertEqual(munged_pofile[1].msgstr, u'Ǿƞḗ')
-        self.assertEqual(munged_pofile[2].msgstr, u'Ŧẇǿ')
-        self.assertEqual(munged_pofile[3].msgstr, u'Ŧħřḗḗ')
+        self.assertEqual(munged_pofile[0].msgstr, "Ẑḗřǿ")
+        self.assertEqual(munged_pofile[1].msgstr, "Ǿƞḗ")
+        self.assertEqual(munged_pofile[2].msgstr, "Ŧẇǿ")
+        self.assertEqual(munged_pofile[3].msgstr, "Ŧħřḗḗ")
         self.assertEqual(
             munged_pofile[4].msgstr,
-            u'Ẑḗřǿ {name} Ǿƞḗ {confidence:0.2f} Ŧẇǿ Ŧħřḗḗ %(qty)0.2f Ƒǿŭř %(name)s Ƒīṽḗ %(age)d',
+            "Ẑḗřǿ {name} Ǿƞḗ {confidence:0.2f} Ŧẇǿ Ŧħřḗḗ %(qty)0.2f Ƒǿŭř %(name)s Ƒīṽḗ %(age)d",
         )
         self.assertEqual(
             munged_pofile[5].msgstr,
-            u'Ẑḗřǿ Ǿƞḗ {age:3s} Ŧẇǿ Ŧħřḗḗ',
+            "Ẑḗřǿ Ǿƞḗ {age:3s} Ŧẇǿ Ŧħřḗḗ",
         )
+
     def test_blank_munge(self):
         munge_one_file(self.sample_po_path, blank=True)
         munged_pofile = polib.pofile(self.sample_po_path)
 
-        self.assertEqual(munged_pofile[0].msgstr, u'')
-        self.assertEqual(munged_pofile[1].msgstr, u'')
-        self.assertEqual(munged_pofile[2].msgstr, u'')
-        self.assertEqual(munged_pofile[3].msgstr, u'')
+        self.assertEqual(munged_pofile[0].msgstr, "")
+        self.assertEqual(munged_pofile[1].msgstr, "")
+        self.assertEqual(munged_pofile[2].msgstr, "")
+        self.assertEqual(munged_pofile[3].msgstr, "")
 
         untranslated_count = len(munged_pofile.untranslated_entries())
         total_count = len(munged_pofile)
@@ -65,32 +63,30 @@ class TestMungePoFile(PoxxTestCase):
         munge_one_file(self.sample_po_path, blank=False, canon_name=self.canon_po_path)
         munged_pofile = polib.pofile(self.sample_po_path)
 
-        self.assertEqual(munged_pofile[0].msgstr, u'Ẑḗřǿ')
-        self.assertEqual(munged_pofile[1].msgstr, u'Uno')
-        self.assertEqual(munged_pofile[2].msgstr, u'Ŧẇǿ')
-        self.assertEqual(munged_pofile[3].msgstr, u'Tree')
+        self.assertEqual(munged_pofile[0].msgstr, "Ẑḗřǿ")
+        self.assertEqual(munged_pofile[1].msgstr, "Uno")
+        self.assertEqual(munged_pofile[2].msgstr, "Ŧẇǿ")
+        self.assertEqual(munged_pofile[3].msgstr, "Tree")
 
     def test_canonical_and_blank_munge(self):
         munge_one_file(self.sample_po_path, blank=True, canon_name=self.canon_po_path)
         munged_pofile = polib.pofile(self.sample_po_path)
 
-        self.assertEqual(munged_pofile[0].msgstr, u'')
-        self.assertEqual(munged_pofile[1].msgstr, u'Uno')
-        self.assertEqual(munged_pofile[2].msgstr, u'')
-        self.assertEqual(munged_pofile[3].msgstr, u'Tree')
+        self.assertEqual(munged_pofile[0].msgstr, "")
+        self.assertEqual(munged_pofile[1].msgstr, "Uno")
+        self.assertEqual(munged_pofile[2].msgstr, "")
+        self.assertEqual(munged_pofile[3].msgstr, "Tree")
 
         untranslated_count = len(munged_pofile.untranslated_entries())
         self.assertEqual(4, untranslated_count)
 
 
 class DiffTestCase(PoxxTestCase):
-
     def setUp(self):
         super().setUp()
-        self.diff_path = os.path.join(self.data_path, 'sample_diff.po')
+        self.diff_path = os.path.join(self.data_path, "sample_diff.po")
 
     def tearDown(self):
-
         super().tearDown()
 
         # Clean up a diff file if the test created one
@@ -105,8 +101,8 @@ class DiffTestCase(PoxxTestCase):
         diff_pofile = polib.pofile(self.diff_path)
 
         # Make sure our non-canonical entries are the ones in the diff
-        self.assertEqual(diff_pofile[0].msgid, u'Zero')
-        self.assertEqual(diff_pofile[1].msgid, u'Two')
+        self.assertEqual(diff_pofile[0].msgid, "Zero")
+        self.assertEqual(diff_pofile[1].msgid, "Two")
 
         # all entries in a diff file should be marked as untranslated
         untranslated_count = len(diff_pofile.untranslated_entries())
