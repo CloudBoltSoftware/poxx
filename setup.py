d76901110e546ba01ee070bde0eac2bc6fb1ffb4
E Einowski <eeinowski@cloudbolt.io>
2024-01-31 19:42:35 -0800

    🔧 refactor(setup.py): use version tuple for VERSION_STR and extras_require for test dependencies 🔖 chore(setup.py): specify Python compatibility with python_requires The version string is now generated from the version tuple, which makes it easier to manage and update the version number. The test dependencies are now specified in extras_require, which is a more standard way of specifying optional dependencies in Python. The Python compatibility is now explicitly specified with python_requires, which helps users to know the Python versions that are compatible with this package.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 version = (1, 1, 1)
-VERSION_STR = '.'.join(str(v) for v in (1,1,0))
+VERSION_STR = '.'.join(str(v) for v in version)  # Use version tuple here
 
 install_requires = [
     'polib',
 ]
 
-test_requires = ['pytest']
+extras_require = {
+    'test': ['pytest'],
+}
 
 setup(
     name='poxx',
@@ -20,11 +22,15 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     install_requires=install_requires,
-    test_requires=test_requires,
+    extras_require=extras_require,  # Use extras_require for test dependencies
     license='BSD',
     include_package_data=True,
     scripts=['poxx.py'],
     classifiers=[
         'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
     ],
+    python_requires='>=3.6',  # Specify your Python compatibility
 )
+
